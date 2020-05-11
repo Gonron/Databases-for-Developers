51f29c92d872274471f5a9200fe97fa464e5bdf7
Gonron <lundsgaard1997@hotmail.dk>
2020-05-11 12:27:18 +0200

    Finished A6 Redis

---
+++ Project/MP02/RedisCSV/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using StackExchange.Redis;
+
+namespace RedisCSV {
+    class Program {
+        public static void Main(string[] args) {
+            var path = @"C:\CPHBusiness\Databases-for-Developers\Project\MP02\RedisCSV\";
+            var lines = File.ReadAllLines(path + "data.csv");
+            Console.WriteLine("Writing");
+            Console.WriteLine(lines.Length);
+            var muxer = ConnectionMultiplexer.Connect("localhost");
+            var conn = muxer.GetDatabase(1);
+
+
+            for (var i = 1; i < lines.Length; i++) {
+                var sep = lines[i].Split(',');
+                conn.HashSet($"order:{sep[0]}", new HashEntry[] {
+                    new HashEntry("StockCode", sep[1]),
+                    new HashEntry("Description", sep[2]),
+                    new HashEntry("Quantity", sep[3]),
+                    new HashEntry("InvoiceDate", sep[4]),
+                    new HashEntry("UnitPrice", sep[5]),
+                    new HashEntry("CustomerID", sep[6]),
+                    new HashEntry("Country", sep[7]),
+                });
+            }
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
---
+++ Redis/Day3/README.md
@@ -0,0 +1,3 @@
+Can't get the service on page 299 (bands.js) to find any bands... Whenever it tries to find something that exists, it keeps loading, not giving any response.
+
+Other then that can't seeme to connect it with Neo4J.
